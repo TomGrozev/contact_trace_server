0b4677262bfeebdb096514807c9eda899ad9b9c7
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2021-06-09 13:46:16 +1000

    Update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :contact_trace_server, ContactTraceServerWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "contact-trace.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
