2eb7152c72c08d989c450cc9df16dc6dc0595270
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2021-06-09 14:11:05 +1000

    Fix typo in config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :contact_trace_server, ContactTraceServerWeb.Endpoint,
-  url: [host: "contact-trace.gigalixirapp.com", port: 443],
+  url: [host: "contact-tracer.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
